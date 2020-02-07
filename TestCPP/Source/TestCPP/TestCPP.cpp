8fe825c8ce923adff6820f0b814866a81f0a4c2f
RENCUREL <pierre.rencurel@orange.fr>
2020-02-07 14:15:34 -0500

    Initial Commit
    DONE Created project and first CPP class + update to gitignore
    TODO : Start project

---
+++ TestCPP/Source/TestCPP/TestCPP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestCPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestCPP, "TestCPP" );
